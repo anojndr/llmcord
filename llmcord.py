65fea762dd7f848552466d55aa8c07aad67f8088
anojndr <anojndr@gmail.com>
2025-11-19 03:08:54 +0800

    Set allowed_mentions to not ping replied user
    Updated the Discord bot initialization to set allowed_mentions with replied_user=False, preventing automatic pings to users when replying to messages.

--- llmcord.py
+++ llmcord.py
@@ -46,7 +46,7 @@ last_task_time = 0
 intents = discord.Intents.default()
 intents.message_content = True
 activity = discord.CustomActivity(name=(config.get("status_message") or "github.com/jakobdylanc/llmcord")[:128])
-discord_bot = commands.Bot(intents=intents, activity=activity, command_prefix=None)
+discord_bot = commands.Bot(intents=intents, activity=activity, command_prefix=None, allowed_mentions=discord.AllowedMentions(replied_user=False))
 
 httpx_client = httpx.AsyncClient()
 
